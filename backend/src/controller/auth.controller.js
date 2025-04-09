4f2cd6c284986feba329e8eeffd256eb37ccdaf7
sraj39273@gmail.com <sraj39273@gmail.com>
2025-04-09 08:29:44 +0530

    tailwind change

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -6,11 +6,11 @@ export const authCallback = async (req, res, next) => {
 		const { id, firstName, lastName, imageUrl } = req.body;
 
 		// check if user already exists
-		const user = await User.findOne({ clerkId: id });
+		let user = await User.findOne({ clerkId: id });
 
+		// if not, create the user
 		if (!user) {
-			// signup
-			await User.create({
+			user = await User.create({
 				clerkId: id,
 				fullname: `${firstName || ""} ${lastName || ""}`.trim(),
 				imageUrl,
@@ -19,12 +19,12 @@ export const authCallback = async (req, res, next) => {
 
 		// Generate JWT Token
 		const token = jwt.sign(
-			{ userId: user.clerkId },  // Payload
-			process.env.JWT_SECRET,   // Secret key (store this securely)
-			{ expiresIn: "7d" }       // Token expiration
+			{ userId: user.clerkId },
+			process.env.JWT_SECRET,
+			{ expiresIn: "7d" }
 		);
 
-		res.status(200).json({ success: true ,token});
+		res.status(200).json({ success: true, token });
 	} catch (error) {
 		console.log("Error in auth callback", error);
 		next(error);
