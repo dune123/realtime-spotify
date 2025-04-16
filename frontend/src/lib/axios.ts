b2e62981af4edbf8bfc4c64339d3a53fd95205b4
sraj39273@gmail.com <sraj39273@gmail.com>
2025-04-16 16:31:36 +0530

    done

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-/*export const axiosInstance = axios.create({
-	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-});*/
-
-
 export const axiosInstance = axios.create({
+	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+});
+
+
+/*export const axiosInstance = axios.create({
 	baseURL:"realtime-spotify-production.up.railway.app/api", // replace with actual URL
-  });
+  });*/
   
